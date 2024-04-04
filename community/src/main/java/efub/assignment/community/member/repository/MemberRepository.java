031dfb94aec7a6a5f46fe8d78afa62093a6d1013
Chaerin Hwang <chaa00@naver.com>
2024-04-05 01:38:02 +0900

    ✨ feat: Member 레포지토리 작성

---
+++ community/src/main/java/efub/assignment/community/member/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package efub.assignment.community.member.repository;
+
+import efub.assignment.community.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+}
