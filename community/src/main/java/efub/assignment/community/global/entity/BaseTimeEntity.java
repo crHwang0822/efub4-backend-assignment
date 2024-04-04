f418f6d5437d7ce64784f964b3a6f11893999ed1
Chaerin Hwang <chaa00@naver.com>
2024-04-05 01:35:14 +0900

    ✨ feat: Member 엔티티 클래스 작성

---
+++ community/src/main/java/efub/assignment/community/global/entity/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package efub.assignment.community.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+    @CreatedDate
+    @Column(name = "reg_date", updatable = false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(name = "mod_date")
+    private LocalDateTime modDate;
+}
---
+++ community/src/main/java/efub/assignment/community/member/domain/Member.java
@@ -0,0 +1,37 @@
+package efub.assignment.community.member.domain;
+
+import efub.assignment.community.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", updatable = false)
+    private Long memberId;
+
+    @Column(nullable = false, length = 60)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String nickname;
+
+    @Column(nullable = false, updatable = false, length = 20)
+    private String university;
+
+    @Column(name = "student_id", nullable = false, updatable = false, length = 10)
+    private String studentId;
+}
